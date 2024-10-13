a0470e66581a7c35f955a627b436e51ce280785c
VedPrakash483 <ved.prakash2021@vitstudent.ac.in>
2024-10-13 15:19:56 +0530

    Added zod input verification

--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,35 @@
 const express =require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
-const {createTodo, updateTodo } = require('../types.js')
+const {createTodo, updateTodo } = require('./types')
 
 
 app.use(express.json())
 
 app.get("/todos", (req, res)=> {
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload);
+    if(!parsePayload.success){
+        req.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
 
+        return;
+    }
 })
 
 app.post("/todos", (req, res)=> {
     
 })
 
-app.post("/completed", (req, res)=> {
-    
+app.put("/completed", (req, res)=> {
+    const createPayload = req.body;
+    const parsePayload = updateTodo.safeParse(createPayload);
+    if(!parsePayload.success){
+        req.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+
+        return;
+    }
 })
